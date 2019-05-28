121ffdfc8fbcc1b31a679e3f3676b4d8a04b9ee9
Cara Steinberg <caralin3@gmail.com>
2019-05-28 14:44:56 -0400

    Add navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navigation extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
