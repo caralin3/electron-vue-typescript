73ba4d4f4de8fd8554055794725a3c16bdf36140
Cara Steinberg <caralin3@gmail.com>
2019-06-05 15:43:38 -0400

    Clean up code

--- src/models/mock.ts
+++ src/models/mock.ts
@@ -5,72 +5,72 @@ export const mockAlbums: Album[] = [
     createdAt: new Date().toISOString(),
     id: '1',
     tags: ['graduation'],
-    title: 'Cornell',
+    title: 'Cornell'
   },
   {
     createdAt: new Date().toISOString(),
     id: '2',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '3',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '4',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '5',
     tags: ['graduation'],
-    title: 'Cornell',
+    title: 'Cornell'
   },
   {
     createdAt: new Date().toISOString(),
     id: '6',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '7',
     tags: ['graduation'],
-    title: 'Cornell',
+    title: 'Cornell'
   },
   {
     createdAt: new Date().toISOString(),
     id: '8',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '9',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '10',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '11',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   },
   {
     createdAt: new Date().toISOString(),
     id: '12',
     tags: [],
-    title: 'An Album',
+    title: 'An Album'
   }
 ];
